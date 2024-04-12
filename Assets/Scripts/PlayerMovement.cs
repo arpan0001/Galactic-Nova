0f45ac6b933cd4b154cb781719958639846f256c
arpan0001 <arpan21csu561@ncuindia.edu>
2024-04-12 15:59:57 +0530

    commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]float movementSpeed = 100;
+
+    Transform myT;
+
+    void Awake()
+    {
+        myT = transform;
+
+    }
+
+    void Update()
+    {
+        Thrust();
+    }
+
+    void Thrust()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+            myT.position += myT.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+    }
+
+
+
+
+
+
+}
