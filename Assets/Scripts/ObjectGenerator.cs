fcad6d41fcc442b1ded99d97a3bb68bcefed9c3a
arpan0001 <arpan21csu561@ncuindia.edu>
2024-04-23 16:43:31 +0530

    commit

--- Assets/Scripts/ObjectGenerator.cs
+++ Assets/Scripts/ObjectGenerator.cs
@@ -1,19 +1,43 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ObjectGenerator : MonoBehaviour
 {
     public GameObject asteroidPrefab;
     public GameObject pickupPrefab;
+    public int initialAsteroidsCount = 50;
+    public int initialPickupsCount = 10;
 
-    private float minX = -5f;
-    private float maxX = 5f;
-    private float minY = -5f;
-    private float maxY = 5f;
+    private float minX = -200f;
+    private float maxX = 300f;
+    private float minY = -100f;
+    private float maxY = 200f;
+    private float minZ = -50f; // Minimum z-axis value
+    private float maxZ = 50f; // Maximum z-axis value
+    private List<Vector3> spawnedPositions = new List<Vector3>();
 
     void Start()
     {
+        GenerateInitialObjects();
         StartCoroutine(GenerateObjects());
+    }
+
+    void GenerateInitialObjects()
+    {
+        for (int i = 0; i < initialAsteroidsCount; i++)
+        {
+            Vector3 asteroidPosition = GetRandomPosition();
+            spawnedPositions.Add(asteroidPosition);
+            Instantiate(asteroidPrefab, asteroidPosition, Quaternion.identity);
+        }
+
+        for (int i = 0; i < initialPickupsCount; i++)
+        {
+            Vector3 pickupPosition = GetRandomPosition();
+            spawnedPositions.Add(pickupPosition);
+            Instantiate(pickupPrefab, pickupPosition, Quaternion.identity);
+        }
     }
 
     IEnumerator GenerateObjects()
@@ -21,14 +45,38 @@ public class ObjectGenerator : MonoBehaviour
         while (true)
         {
             // Generate asteroid
-            Vector3 asteroidPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+            Vector3 asteroidPosition = GetRandomPosition();
+            spawnedPositions.Add(asteroidPosition);
             Instantiate(asteroidPrefab, asteroidPosition, Quaternion.identity);
 
             // Generate pickup
-            Vector3 pickupPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+            Vector3 pickupPosition = GetRandomPosition();
+            spawnedPositions.Add(pickupPosition);
             Instantiate(pickupPrefab, pickupPosition, Quaternion.identity);
 
-            yield return new WaitForSeconds(2f); // Wait for 2 seconds before generating next objects
+            yield return new WaitForSeconds(6f); // Wait for 1 second before generating next objects
         }
+    }
+
+    Vector3 GetRandomPosition()
+    {
+        Vector3 position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
+        while (!IsPositionValid(position))
+        {
+            position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
+        }
+        return position;
+    }
+
+    bool IsPositionValid(Vector3 position)
+    {
+        foreach (Vector3 spawnedPosition in spawnedPositions)
+        {
+            if (Vector3.Distance(position, spawnedPosition) < 5f)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
